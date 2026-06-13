4bbc2493967e207bf8f85038498e72af8fabcf9e
unknown <stevanuswahyu2707@gmail.com>
2026-06-13 23:10:33 +0700

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="py-8 text-center border-t border-slate-700 bg-slate-900">
       <p>
-        © 2026 Stevanus Wahyu. All Rights Reserved.
+        
       </p>
     </footer>
   );
